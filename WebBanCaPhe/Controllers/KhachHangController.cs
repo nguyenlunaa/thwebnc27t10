1ca26a8a37e04ca77f095493df91fd9c6830724a
Nguyễn Trung Nguyên <nguyen12072kk2@gmail.com>
2022-10-27 08:48:52 +0700

    webnc27t10

---
+++ WebBanCaPhe/Controllers/KhachHangController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebBanCaPhe.Models;
+
+namespace WebBanCaPhe.Controllers
+{
+    public class KhachHangController : Controller
+    {
+        // GET: KhachHang
+        QuanLyCaPheEntities db = new QuanLyCaPheEntities();
+        public ActionResult Index()
+        {
+            var lstKH = from KH in db.KhachHangs select KH;
+
+            return View(lstKH);
+        }
+    }
+}
